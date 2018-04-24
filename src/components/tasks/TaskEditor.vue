19cbdf4c601db5b245404d6f5bb8bb1c13740f1a
John Waweru Wambugu <wambugu.john.waweru@outlook.com>
2018-04-24 17:15:40 +0300

    added a task-editor component. (no implementation)

---
+++ src/components/tasks/TaskEditor.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-dialog v-model="dialog" persistent max-width="450px" lazy transition="dialog-bottom-transition">
+
+        <v-card tile>
+            <v-card-title>
+                Add a new Task
+            </v-card-title>
+        </v-card>
+
+    </v-dialog>  
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate';
+
+export default {
+    name: 'TaskEditorComponent',
+    mixins: [validationMixin],
+    props: {
+        dialog: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data: () => ({}),
+    methods: {
+        save() {
+            //
+        },
+        close() {
+            this.$v.reset();
+            this.$emit('close:add-task-editor');
+        }
+    }
+}
+</script>
